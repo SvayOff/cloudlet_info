50c39c39893e4b250e741696b34595caed887f76
SvayOff <marsiul2001@gmail.com>
2023-03-08 15:48:57 +0200

    added comp: Week and styles for him

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import WeatherTop from '../components/WeatherTop';
+import Week from './Week';
 
 const Weather: React.FC = () => {
   return (
     <div className="weather">
       <WeatherTop />
+      <Week />
     </div>
   );
 };
