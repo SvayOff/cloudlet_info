1b8ebf6111a828b8e79807c7e61cd2ab76cd0d2f
SvayOff <marsiul2001@gmail.com>
2023-03-20 18:35:10 +0200

    added removeFromFavorites to WeatherToday & styles

--- src/components/WeatherToday.tsx
+++ src/components/WeatherToday.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
-import { setWeatherFavorites } from '../redux/slices/weatherSlice';
+import { setWeatherFavorites, removeWeatherFromFavorites } from '../redux/slices/weatherSlice';
 import { NavLink } from 'react-router-dom';
 
 export const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
@@ -38,6 +38,7 @@ export const weatherTodayImage = (status: string | null) => {
 const WeatherToday: React.FC = () => {
   const dispatch = useDispatch();
   const weatherToday = useSelector((state: RootState) => state.weatherSlice.weatherToday);
+  const weatherFavorites = useSelector((state: RootState) => state.weatherSlice.weatherFavorites);
 
   const weatherTodayCity = weatherToday && weatherToday.name;
   const weatherTodayTemp = weatherToday && Math.round(weatherToday.main.temp);
@@ -52,9 +53,15 @@ const WeatherToday: React.FC = () => {
   const weatherTodayMonth =
     weatherToday &&
     months.filter((month, index) => (index === new Date().getMonth() ? month : null));
+  const hasWeatherInFavorites =
+    weatherToday && weatherFavorites.find((weather) => weather.id === weatherToday.id);
 
   const addToFavorite = () => {
     weatherToday && dispatch(setWeatherFavorites(weatherToday));
+  };
+
+  const removeFromFavorites = () => {
+    weatherToday && dispatch(removeWeatherFromFavorites(weatherToday.id));
   };
 
   return (
@@ -91,17 +98,27 @@ const WeatherToday: React.FC = () => {
           />
         </div>
       </NavLink>
-      <button className="today__favorite" onClick={addToFavorite}>
-        <svg viewBox="0 0 128 128" xmlSpace="preserve" xmlns="http://www.w3.org/2000/svg">
-          <g id="Layer_2">
-            <path
-              className="st2"
-              d="M57.3 32.7c-9.2-9.2-24.1-9.2-33.3 0-9.2 9.2-9.2 24.1 0 33.3l6.7 6.7L64 106l33.3-33.3L104 66c9.2-9.2 9.2-24.1 0-33.3s-24.1-9.2-33.3 0L64 39.4"
-            />
-            <path className="st2" d="M29.1 49.1c0-6.9 5.6-12.4 12.4-12.4" />
-          </g>
-        </svg>
-      </button>
+      {hasWeatherInFavorites ? (
+        <button className="today__favorite today__favorite-remove" onClick={removeFromFavorites}>
+          <svg viewBox="0 0 80 80" xmlSpace="preserve" xmlns="http://www.w3.org/2000/svg">
+            <title />
+            <path d="M40 5.4C20.9 5.4 5.4 20.9 5.4 40s15.5 34.6 34.5 34.6S74.5 59.1 74.6 40C74.5 20.9 59.1 5.5 40 5.4zm0 66.2C22.6 71.6 8.4 57.4 8.4 40 8.4 22.6 22.6 8.4 40 8.4c17.4 0 31.6 14.1 31.6 31.5-.1 17.5-14.2 31.6-31.6 31.7z" />
+            <path d="M40 14.9c-13.8 0-25.1 11.2-25.1 25.1h3c0-12.2 9.9-22.1 22.1-22.1v-3zM49.2 28.7 40 37.9l-9.2-9.2-2.1 2.1 9.2 9.2-9.2 9.2 2.1 2.1 9.2-9.2 9.2 9.2 2.1-2.1-9.2-9.2 9.2-9.2z" />
+          </svg>
+        </button>
+      ) : (
+        <button className="today__favorite" onClick={addToFavorite}>
+          <svg viewBox="0 0 128 128" xmlSpace="preserve" xmlns="http://www.w3.org/2000/svg">
+            <g id="Layer_2">
+              <path
+                className="st2"
+                d="M57.3 32.7c-9.2-9.2-24.1-9.2-33.3 0-9.2 9.2-9.2 24.1 0 33.3l6.7 6.7L64 106l33.3-33.3L104 66c9.2-9.2 9.2-24.1 0-33.3s-24.1-9.2-33.3 0L64 39.4"
+              />
+              <path className="st2" d="M29.1 49.1c0-6.9 5.6-12.4 12.4-12.4" />
+            </g>
+          </svg>
+        </button>
+      )}
     </div>
   );
 };
